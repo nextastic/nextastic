6e4ddc1f5418bd70b41665983c3b6adc062ce6b0
Mike Wu <hello@mikewu.dev>
2024-05-19 22:17:07 +0900

    remove workspace changeset

--- boilerplates/aws/src/app/page.tsx
+++ boilerplates/aws/src/app/page.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image'
-import { createRoute } from '@nextastic/http'
 
 export default function Home() {
-  createRoute()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
