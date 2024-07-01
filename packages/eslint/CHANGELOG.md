a5cd10e8387e1c9886895b3ba3ca487428b493e1
Mike Wu <hello@mikewu.dev>
2024-07-01 17:23:15 +0700

    fix(eslint): export object

--- packages/eslint/CHANGELOG.md
+++ packages/eslint/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @nextastic/eslint
 
+## 0.2.0
+
+### Minor Changes
+
+- export eslint object
+
 ## 0.1.0
 
 ### Minor Changes
--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,20 +1,18 @@
 // index.js
-export default [
-  {
-    extends: ['next/core-web-vitals', 'plugin:jest/recommended'],
-    plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
-    rules: {
-      'no-console': 'warn',
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-        },
-      ],
-      'jest/no-try-expect': 'off',
-      'jest/no-test-callback': 'off',
-      'jest/no-conditional-expect': 'off',
-    },
+export default {
+  extends: ['next/core-web-vitals', 'plugin:jest/recommended'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'jest'],
+  rules: {
+    'no-console': 'warn',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+    'jest/no-try-expect': 'off',
+    'jest/no-test-callback': 'off',
+    'jest/no-conditional-expect': 'off',
   },
-]
+}
