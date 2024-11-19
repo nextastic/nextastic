24bf6684912e3e413c370f83f69f3351ec083e54
Mike Wu <hello@mikewu.dev>
2024-11-20 01:58:44 +0900

    feat(Queue): better logging (#49)

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -2,4 +2,13 @@ import pino from 'pino'
 
 export const logger = pino({
   level: process.env.NEXT_PUBLIC_LOG_LEVEL,
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      ignore: 'pid,hostname',
+      messageFormat: '{msg}',
+      translateTime: false
+    }
+  }
 })
