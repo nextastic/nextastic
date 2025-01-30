9e3b62c9549b99b62e777363aecb73705271b585
Mike Wu <hello@mikewu.dev>
2025-01-30 23:21:44 +0900

    setup jest and tests

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+export default {
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+        tsconfig: {
+          module: 'ESNext',
+        },
+      },
+    ],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[tj]s?(x)'],
+  roots: ['<rootDir>/packages'],
+}
